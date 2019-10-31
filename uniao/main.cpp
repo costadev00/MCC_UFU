3429d13443cbac7f257a13763d9f8c41d9c9a287
Matheus Costa <43221564+mcosta00@users.noreply.github.com>
2019-10-31 11:29:26 -0300

    uniao done

---
+++ uniao/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int a[4] = {1,2,3,4}, b[4] = {3,4,5,6};
+    int c[8], k=0, existe;
+
+    for(int i=0; i<4; i++) {
+        existe=0;
+        for(int j=0; j<k; j++) {
+            if (a[i] == c[j])
+                existe=1;
+        }
+        if(existe == 0)
+            c[k++] = a[i];
+    }
+
+    for(int i=0; i<4; i++) {
+        existe=0;
+        for(int j=0; j<k; j++) {
+            if (b[i] == c[j])
+                existe=1;
+        }
+        if(existe == 0)
+            c[k++] = b[i];
+    }
+
+
+    for(int i=0; i<k; i++) {
+        printf("%d ", c[i]);
+    }
+    printf("\n");
+    return 0;
+}
